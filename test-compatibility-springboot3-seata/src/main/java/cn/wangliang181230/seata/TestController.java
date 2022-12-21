da3c899a5f9ae2d4be3bc15183642b9f75b3df52
王良 <841369634@qq.com>
2022-12-21 12:27:52 +0800

    添加springboot打包插件。

--- test-compatibility-springboot3-seata/src/main/java/cn/wangliang181230/seata/TestController.java
+++ test-compatibility-springboot3-seata/src/main/java/cn/wangliang181230/seata/TestController.java
@@ -1,10 +1,14 @@
 package cn.wangliang181230.seata;
 
+import io.seata.core.context.RootContext;
 import io.seata.spring.annotation.GlobalTransactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 @RestController
 public class TestController {
@@ -12,10 +16,19 @@ public class TestController {
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 
-	@GetMapping("/test")
+	private static final AtomicInteger i = new AtomicInteger(0);
+
+	@GetMapping("/")
 	@GlobalTransactional
-	public Object test() {
-		jdbcTemplate.execute("insert into tb_order (id) values (1)");
+	public Object test(@RequestParam(value = "testError", required = false) Boolean testError) {
+		String xid = RootContext.getXID();
+		System.out.println(xid);
+
+		if (Boolean.TRUE.equals(testError)) {
+			throw new RuntimeException("测试异常情况");
+		}
+
+		jdbcTemplate.execute("insert into tb_order (id) values (" + i.incrementAndGet() + ")");
 
 		return "test";
 	}
