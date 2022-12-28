358e4119d493dd42f822765c7584e9ecc99a1f52
王良 <841369634@qq.com>
2022-12-28 11:20:23 +0800

    启动脚本小调整。

--- study-native-image/study-native-image-with-springboot3/startup-exe.bat
+++ study-native-image/study-native-image-with-springboot3/startup-exe.bat
@@ -1,5 +1,5 @@
 title "study-native-image-with-springboot2"
 
-start target/study-native-image-with-springboot2.exe --server.port=8091
+start target/study-native-image-with-springboot3.exe --server.port=8091
 
 cmd
