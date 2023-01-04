2d9d9f3f01fdf2eaad2186ee8257ee19b9f64a71
王良 <841369634@qq.com>
2023-01-04 11:53:49 +0800

    注解未添加运行时的BUG修复。

--- study-native-image/study-native-image-with-springboot3/study-native-image-with-springboot3-application/src/main/java/cn/wangliang181230/studynativeimage/MyAnnotation.java
+++ study-native-image/study-native-image-with-springboot3/study-native-image-with-springboot3-application/src/main/java/cn/wangliang181230/studynativeimage/MyAnnotation.java
@@ -1,4 +1,11 @@
 package cn.wangliang181230.studynativeimage;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE, ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface MyAnnotation {
 }
