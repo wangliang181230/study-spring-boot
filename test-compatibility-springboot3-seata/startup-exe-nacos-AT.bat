55ce64894bcb02b5c3fcd71958b65790077636d9
王良 <841369634@qq.com>
2023-02-10 10:32:41 +0800

    脚本调整。

--- test-compatibility-springboot3-seata/startup-exe-nacos-AT.bat
+++ test-compatibility-springboot3-seata/startup-exe-nacos-AT.bat
@@ -2,10 +2,12 @@ title "test-compatibility-springboot3-seata"
 
 start target/test-compatibility-springboot3-seata.exe ^
 	-Dserver.port=8081 ^
+^
 	-Dlogging.level.io.seata=DEBUG ^
 	-Dlogging.level.io.seata.spring.aot.AotUtils=INFO ^
 	-Dlogging.level.io.seata.core.rpc=INFO ^
-	-Dseata.data-source-proxy-mode=AT ^
+^
 	-Dseata.registry.type=nacos ^
-	-Dseata.registry.nacos.server-addr=192.168.1.103:8848
+^
+	-Dseata.data-source-proxy-mode=AT
 
